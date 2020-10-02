6e9280c89114ca7b30f323be5215a6d5bdf49d5f
Marcos Velasco <devslife7@gmail.com>
2020-10-01 21:13:38 -0400

    fixed project details

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -8,11 +8,12 @@ function ProjectCard({ project }) {
 
   return (
     <div>
-      <h2>{project.name}</h2>
+      <h2>
+        {project.name} - ({project.completion_percentage})
+      </h2>
       <p>Description: {project.description}</p>
       {/* <p>start date: {project.start_date}</p> */}
-      {/* <p>deadline: {project.deadline}</p> */}
-      <p>completion percentage: {project.completion_percentage}</p>
+      <p>deadline: {project.deadline}</p>
       {/* <p>created at: {project.created_at}</p>
       <p>updated at: {project.updated_at}</p> */}
       <Link to='/project/details' onClick={handleLink}>
