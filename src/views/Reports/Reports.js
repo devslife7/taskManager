dec9454e2d37f8695e77e253a1819f7ea74cb4b3
Marcos Velasco <devslife7@gmail.com>
2021-07-14 01:45:04 -0400

    added create report button and started with the reports container

--- src/views/Reports/Reports.js
+++ src/views/Reports/Reports.js
@@ -1,5 +1,34 @@
+import { Button, makeStyles } from '@material-ui/core'
 import React from 'react'
+import AddIcon from '@material-ui/icons/Add'
+
+const useStyles = makeStyles(() => ({
+  createBtn: {
+    margin: '7rem 0 0 10rem',
+  },
+  reportsContainer: {
+    margin: '7rem 0 0 10rem',
+    backgroundColor: '#f4f4f4',
+  },
+}))
 
 export default function Reports() {
-  return <div style={{ margin: '100px auto', fontSize: '2rem' }}>Reports page coming soon...</div>
+  const classes = useStyles()
+  return (
+    <div>
+      <div className={classes.createBtn}>
+        <Button
+          variant='contained'
+          color='primary'
+          startIcon={<AddIcon style={{ fontSize: '1.4rem' }} />}
+          style={{ fontSize: '1.2rem' }}
+          // onClick={handleOpenDialog}
+        >
+          Create Report
+        </Button>
+      </div>
+
+      <div className={classes.reportsContainer}>Reports here</div>
+    </div>
+  )
 }
