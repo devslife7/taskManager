1042a4e686ff413812d8aa3e8b7bd746808271fb
Marcos Velasco <devslife7@gmail.com>
2021-07-16 18:30:38 -0400

    moved the reducers to redux folder

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import rootReducer from './reducers/index'
+import rootReducer from './redux/reducers/index'
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
