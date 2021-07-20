2c4a5871cf920055510b28dfd84a70188a92e0ae
Marcos Velasco <devslife7@gmail.com>
2021-07-19 23:12:46 -0400

    disaled the material ui ripple effect on the whole application

--- src/index.js
+++ src/index.js
@@ -45,6 +45,13 @@ const theme = createMuiTheme({
       },
     },
   },
+  props: {
+    // Name of the component
+    MuiButtonBase: {
+      // The properties to apply
+      disableRipple: true, // No more ripple, on the whole application!
+    },
+  },
 })
 
 ReactDOM.render(
