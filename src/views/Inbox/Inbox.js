b3bfe855c5896ad31432d8e500743aa97ddd99f1
Marcos Velasco <devslife7@gmail.com>
2021-07-21 21:46:25 -0400

    cleaned up styles on inbox

--- src/views/Inbox/Inbox.js
+++ src/views/Inbox/Inbox.js
@@ -1,11 +1,19 @@
+import { makeStyles } from '@material-ui/core'
 import React from 'react'
-import { format, fromUnixTime } from 'date-fns'
-import { formatDuration, intervalToDuration } from 'date-fns'
+
+const useStyles = makeStyles(() => ({
+  container: {
+    margin: '100px auto',
+    fontSize: '2rem',
+    backgroundColor: 'yellow',
+  },
+}))
 
 export default function Inbox() {
+  const classes = useStyles()
   return (
     <>
-      <div style={{ margin: '100px auto', fontSize: '2rem', backgroundColor: 'yellow' }}>
+      <div className={classes.container}>
         <div>Inbox Page coming soon...</div>
         <div>format Exel sheets</div>
       </div>
